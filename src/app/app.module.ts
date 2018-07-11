09284a7a194d93cecb7ca3e1ff8db071e45db98d
MaksymI <maksym.izmailov.lv@gmail.com>
2018-07-11 14:32:43 +0300

    component reexport

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material/material.module';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
-import { Page404Component } from './page404/page404.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
+import {
+  FooterComponent,
+  HeaderComponent,
+  Page404Component,
+  HomeComponent,
+  LoginComponent
+ } from '.';
 
 @NgModule({
   declarations: [
---
+++ src/app/index.ts
@@ -0,0 +1,5 @@
+export * from './footer/footer.component';
+export * from './header/header.component';
+export * from './page404/page404.component';
+export * from './home/home.component';
+export * from './login/login.component';
