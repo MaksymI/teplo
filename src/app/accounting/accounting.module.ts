63abea64ce5e330df37710b5f55f6110543919b0
MaksymI <maksym.izmailov.lv@gmail.com>
2018-07-17 13:24:43 +0300

    record component

--- src/app/accounting/accounting.module.ts
+++ src/app/accounting/accounting.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MaterialModule } from '../material/material.module';
 import { AccountingRoutingModule } from './accounting-routing.module';
 import { RecordListComponent } from './components/record-list/record-list.component';
+import { RecordComponent } from './components/record/record.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    MaterialModule,
     AccountingRoutingModule
   ],
-  declarations: [RecordListComponent]
+  declarations: [RecordListComponent, RecordComponent]
 })
 export class AccountingModule { }
---
+++ src/app/accounting/components/record/record.component.ts
@@ -0,0 +1,29 @@
+import {
+  Component,
+  ChangeDetectionStrategy,
+  Input,
+  Output,
+  EventEmitter
+ } from '@angular/core';
+ import { Record } from '../../models/record.model';
+
+@Component({
+  selector: 'app-record',
+  templateUrl: './record.component.html',
+  styleUrls: ['./record.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class RecordComponent {
+  @Input() record: Record;
+
+  @Output() saveRecord = new EventEmitter<Record>();
+  @Output() editRecord = new EventEmitter<Record>();
+
+  onSaveRecord(): void {
+    this.saveRecord.emit(this.record);
+  }
+
+  onEditRecord(): void {
+    this.editRecord.emit(this.record);
+  }
+}
