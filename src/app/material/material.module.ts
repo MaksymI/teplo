63d7ff98d278d9bfff16e57376b7b2110d0c01a7
MaksymI <maksym.izmailov.lv@gmail.com>
2018-07-17 13:15:37 +0300

    same formatting

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 
-import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-
-
 
 @NgModule({
   imports: [
