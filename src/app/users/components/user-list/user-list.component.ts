1755cef53c649767c7bbcf17f0db01cf25c42617
MaksymI <maksym.izmailov.lv@gmail.com>
2018-07-19 19:47:04 +0300

    react in user-list for editedUserID from user-form

--- src/app/users/components/user-list/user-list.component.ts
+++ src/app/users/components/user-list/user-list.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 import { Observable, of, throwError } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 import { User } from '../../models/user.model';
 import { UserArrayService } from '../../services/user-array.service';
+import { pipe } from '../../../../../node_modules/@angular/core/src/render3/pipe';
 
 
 @Component({
@@ -15,6 +17,8 @@ import { UserArrayService } from '../../services/user-array.service';
 export class UserListComponent implements OnInit {
   users$: Observable<User[]>;
 
+  private editedUser: User;
+
   constructor(
     private router: Router,
     private route: ActivatedRoute,
@@ -23,6 +27,22 @@ export class UserListComponent implements OnInit {
 
   ngOnInit() {
     this.users$ = this.userArrayService.getUsers();
+
+    this.route.paramMap
+      .pipe(
+        switchMap((params: Params) =>
+          this.userArrayService.getUser(+params.get('editedUserID'))
+        )
+      )
+      .subscribe(
+        (user: User) => {
+          this.editedUser = { ...user };
+          console.log(
+            `last time you edited user ${JSON.stringify(this.editedUser)}`
+          );
+        },
+        err => console.log(err)
+      );
   }
 
   onEditUser(user: User) {
@@ -31,6 +51,13 @@ export class UserListComponent implements OnInit {
     // or
     // const link = ['edit', user.id];
     // this.router.navigate(link, {relativeTo: this.route});
+  }
+
+  isEdited(user: User) {
+    if (this.editedUser) {
+      return user.id === this.editedUser.id;
+    }
+    return false;
   }
 
 }
