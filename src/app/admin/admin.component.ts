2d699dd097e657fbd9281ac203258d025c1eabdb
MaksymI <maksym.izmailov.lv@gmail.com>
2018-07-19 23:26:19 +0300

    mat-tab-nav-bar in AdminComponent

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -6,8 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
-  links = ['Dashboard', 'Manage Tasks', 'Manage Users'];
-  activeLink = this.links[0];
+  navLinks = [
+    {
+      label: 'Dashboard',
+      path: './'
+     },
+     {
+       label: 'Manage Tasks',
+       path: './'
+     },
+     {
+       label: 'Manage Users',
+       path: './'
+     }
+  ];
+
+  activeLink = this.navLinks[0];
 
   constructor() { }
 
