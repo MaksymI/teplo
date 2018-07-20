106e896dd0d0ffc5218c2c4de7f906fbcc816e42
MaksymI <maksym.izmailov.lv@gmail.com>
2018-07-20 17:59:14 +0300

    footer using mat-tab-nav-bar

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -8,6 +8,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   imports: [
@@ -18,7 +19,8 @@ import { MatTabsModule } from '@angular/material/tabs';
     MatButtonModule,
     MatIconModule,
     MatListModule,
-    MatTabsModule
+    MatTabsModule,
+    MatDividerModule
   ],
   exports: [
     MatToolbarModule,
@@ -28,7 +30,8 @@ import { MatTabsModule } from '@angular/material/tabs';
     MatButtonModule,
     MatIconModule,
     MatListModule,
-    MatTabsModule
+    MatTabsModule,
+    MatDividerModule
   ],
   declarations: []
 })
