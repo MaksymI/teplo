e90b625dce7b57db77bf5b79c697279ad4036a7c
MaksymI <maksym.izmailov.lv@gmail.com>
2018-07-30 17:20:58 +0300

    handling query params & fragment in login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, NavigationExtras } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { MessagesService } from '../services/messages.service';
 import { IUser } from '../models/user.model';
@@ -54,8 +54,14 @@ export class LoginComponent implements OnInit, OnDestroy {
         const redirect = this.authService.redirectUrl
           ? this.authService.redirectUrl
           : '/home';
+
+          const navigationExtras: NavigationExtras = {
+            queryParamsHandling: 'preserve',
+            preserveFragment: true
+          };
+
           // Redirect the user
-          this.router.navigate([redirect]);
+          this.router.navigate([redirect], navigationExtras);
       }
     });
   }
