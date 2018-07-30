487ee253e26bccde8046e787c25d3497c54e9cfc
MaksymI <maksym.izmailov.lv@gmail.com>
2018-07-30 17:40:33 +0300

    showing query params & fargment in dashboard

--- src/app/admin/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-dashboard.component.css']
 })
 export class AdminDashboardComponent implements OnInit {
+  sessionID: Observable<string>;
+  token: Observable<string>;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    // Capture the session ID if available
+    this.sessionID = this.route
+      .queryParamMap
+      .pipe(
+        map(params => params.get('sessionId') || 'None')
+      );
+
+    // Capture the fragment if available
+    this.token = this.route
+      .fragment
+      .pipe(
+        map(fargment => fargment || 'None')
+      );
   }
 
 }
