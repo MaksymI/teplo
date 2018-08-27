11c278dddf9cfc151f74ae075fcb2782a0f312a7
MaksymI <maksym.izmailov.lv@gmail.com>
2018-08-27 19:53:58 +0300

    findOneAndUpdate returns record

--- server/api/record.ts
+++ server/api/record.ts
@@ -3,22 +3,20 @@ import { Record } from '../models/record';
 
 export const getRecords = (req: Request, res: Response) => {
   console.log('getRecords() invoked');
-  Record.find({})
-    .exec((err, records) => {
-      if (err) {
-        console.log('Error retrieving records!');
-        res.status(404).send('Not Found!');
-      } else {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).send(JSON.stringify(records, null, 2));
-      }
-    });
+  Record.find({}).exec((err, records) => {
+    if (err) {
+      console.log('Error retrieving records!');
+      res.status(404).send('Not Found!');
+    } else {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).send(JSON.stringify(records, null, 2));
+    }
+  });
 };
 
 export const getRecordById = (req: Request, res: Response) => {
   console.log('getRecordById() invoked');
-  Record.findById(req.params.recordID)
-  .exec((err, record) => {
+  Record.findById(req.params.recordID).exec((err, record) => {
     if (err) {
       console.log('Error retrieving record!');
       res.status(404).send('Not Found!');
@@ -31,35 +29,35 @@ export const getRecordById = (req: Request, res: Response) => {
 
 export const updateRecordById = (req: Request, res: Response) => {
   console.log('updateRecordById() invoked');
-  Record.update({ _id: req.params.recordID }, req.body)
-  .exec((err, record) => {
-    if (err) {
-      console.log('Error updating record!');
-      res.status(404).send('Not Found!');
-    } else {
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).send(record);
+  Record.findOneAndUpdate({ _id: req.params.recordID }, req.body).exec(
+    (err, record) => {
+      if (err) {
+        console.log('Error updating record!');
+        res.status(404).send('Not Found!');
+      } else {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).send(record);
+      }
     }
-  });
+  );
 };
 
 export const createRecord = (req: Request, res: Response) => {
   console.log('createRecord() invoked');
   Record.create(req.body)
-  .then(record => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).send(record);
-  })
-  .catch(err => {
+    .then(record => {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).send(record);
+    })
+    .catch(err => {
       console.log(err);
       res.status(404).send('Unable to create record!');
-  });
+    });
 };
 
 export const deleteRecordById = (req: Request, res: Response) => {
   console.log('deleteRecordById() invoked');
-  Record.findByIdAndRemove(req.params.recordID)
-  .exec((err, record) => {
+  Record.findByIdAndRemove(req.params.recordID).exec((err, record) => {
     if (err) {
       console.log('Error deleting record!');
       res.status(404).send('Not Found!');
