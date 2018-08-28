4b0b11ede341308f52394dfceef1193582457fc5
MaksymI <maksym.izmailov.lv@gmail.com>
2018-08-28 17:20:20 +0300

    ROUTER-STORE

--- src/app/+store/core-store.module.ts
+++ src/app/+store/core-store.module.ts
@@ -5,16 +5,20 @@ import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { RouterStateSerializerProvider, routerReducers } from '.';
 import { environment } from '../../environments/environment';
 
 @NgModule({
   imports: [
     CommonModule,
-    StoreModule.forRoot({}),
+    StoreModule.forRoot(routerReducers),
     EffectsModule.forRoot([]),
+    StoreRouterConnectingModule.forRoot(),
     // Insrumentation must be imported after importing StoreModule (config is optional)
     !environment.production ? StoreDevtoolsModule.instrument() : []
   ],
-  declarations: []
+  declarations: [],
+  providers: [RouterStateSerializerProvider]
 })
 export class CoreStoreModule { }
